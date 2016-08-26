82b3579df993f19e2f0c5d1f24f2520ba7e28bbf
ruslanlazin <ruslanlazin@gmail.com>
2016-08-26 17:39:05 +0300

    first commit

---
+++ src/ua/pp/lazin/races/GamblerDAOPostgres.java
@@ -0,0 +1,39 @@
+package ua.pp.lazin.races;
+
+import java.sql.*;
+
+/**
+ * Created by Laz on 26.08.2016.
+ */
+public class GamblerDAOPostgres implements GamblerDAO {
+
+    //private DataSource dataPool;
+    private Connection con;
+
+    public GamblerDAOPostgres(Connection con) {
+        this.con = con;
+    }
+
+    public void close() throws SQLException {
+        this.con.close();
+    }
+
+    public Gambler getGamblerById(int id) throws SQLException {
+        PreparedStatement st = con.prepareStatement("SELECT * FROM Gamblers "
+                + "WHERE id = ?");
+        st.setInt(1, id);
+        ResultSet rs = st.executeQuery();
+        Gambler t = null;
+        if (rs.next()) {
+            t = new Gambler();
+            t.setGamblerId(rs.getInt(1));
+            t.setLogin(rs.getString(2));
+        }
+        return t;
+    }
+    public void addGambler(Gambler gambler) throws SQLException {
+        Statement st = con.createStatement();
+        st.execute("INSERT into Gamblers(login, password, balance) VALUES ('vas', 'pas',10)");
+        st.close();
+    }
+}
---
+++ src/ua/pp/lazin/races/Test.java
@@ -0,0 +1,17 @@
+package ua.pp.lazin.races;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Laz on 26.08.2016.
+ */
+public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        DAOFactory.getInstance().getGamblerDAO().addGambler(new Gambler());
+        Gambler vasja = DAOFactory.getInstance().getGamblerDAO().getGamblerById(1);
+        System.out.println(vasja.getLogin());
+    }
+
+
+}
